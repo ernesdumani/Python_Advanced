618d5baecd6d827850ab86af7f2d913985a4cf74
ernesdumani <ernesdumani@gmail.com>
2025-06-04 17:44:01 +0200

    Add files via upload

---
+++ module2/tuple.py
@@ -0,0 +1,15 @@
+words=('spam','eggs','milk')
+
+print(words[0])
+
+#words[1]="cheese"
+
+#print(words)
+
+person=('Amar', 14, 'Student')
+
+name, age, profession=person
+
+print(name, "'s", "profession is", profession, "and he is", age, "years old")
+
+print("hello world!")
